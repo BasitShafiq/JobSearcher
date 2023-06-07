0e07ff26390cc92343d8dd473105e6efe67f20d6
AbdulBasitShafiq <abdulbasit.devblends@gmail.com>
2023-06-07 19:57:22 +0500

    Add project files.

---
+++ WebApplication1/Controllers/LoginController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+namespace WebApplication1.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        [HttpGet]
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Hello";
+            return View();
+        }
+        [HttpPost]
+        public ActionResult test(String email, String password)
+        {
+            User model = new User
+            {
+                email = email,
+                password = password
+            };
+
+            return View(model);
+        }
+       
+    }
+}
